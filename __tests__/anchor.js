3b1738a458dce9085c9b2ce8b6b94f79ac036537
Tyler Long <tyler4long@gmail.com>
2017-02-13 09:35:13 +0800

    Pass all tests

--- __tests__/anchor.js
+++ __tests__/anchor.js
@@ -12,7 +12,7 @@ tape("markdown-it-github-toc anchor", (t) => {
 `,
       {anchorLink: true}
     ),
-  `<p></p>
+  `<p><a href="Title">toc</a></p>
 <h1 id="heading"><a class="markdownIt-Anchor" href="#heading">#</a> 'Heading' ?</h1>
 <h1 id="lel"><a class="markdownIt-Anchor" href="#lel">#</a> $.lel!</h1>
 <h1 id="lel-2"><a class="markdownIt-Anchor" href="#lel-2">#</a> $.lel?</h1>\n`,
